967d6d182919fd7b55b9728bea73a52bb0b26fbf
Benjo <benjoestrella13@gmail.com>
2025-06-01 17:46:44 +0800

    2nd commit

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-<<<<<<< HEAD
 app.use('/public', express.static(path.join(__dirname, 'public')));
-=======
-app.use(express.static(path.join(__dirname, 'public')));
->>>>>>> d8484320019db9853853aba82ae01c5ab9ca1ef0
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -20,28 +16,12 @@ app.get('/dashboard.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
 
-<<<<<<< HEAD
 app.get('/register.html', (req, res ) => {
     res.sendFile(path.join(__dirname, 'register.html'));
 });
 
 app.get('/add-cash.html', (req, res ) => {
     res.sendFile(path.join(__dirname, 'add-cash.html'));
-=======
-app.get('/transfer.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'transfer.html'));
-});
-app.get('/history.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'history.html'));
-});
-
-app.get('/profile.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'profile.html'));
-});
-
-app.get('/register.html', (req, res ) => {
-    res.sendFile(path.join(__dirname, 'register.html'));
->>>>>>> d8484320019db9853853aba82ae01c5ab9ca1ef0
 });
 
 app.get('/history.html', (req, res) => {
@@ -52,7 +32,6 @@ app.get('/send-money.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'send-money.html'));
 });
 
-<<<<<<< HEAD
 app.get('/transfer.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'transfer.html'));
 });
@@ -85,13 +64,10 @@ app.get('/settings.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'settings.html'));
 });
 
-=======
->>>>>>> d8484320019db9853853aba82ae01c5ab9ca1ef0
 app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-<<<<<<< HEAD
 app.get('/help.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'help.html'));
 });
@@ -101,8 +77,6 @@ app.get('/services.html', (req, res) => {
 });
 
 
-=======
->>>>>>> d8484320019db9853853aba82ae01c5ab9ca1ef0
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
